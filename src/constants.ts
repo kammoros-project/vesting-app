708c241c50964f2c816c02b6ad67b2599892eee8
Bob <heycryptobob@gmail.com>
2022-11-04 19:54:52 +0800

    new contract

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,3 @@
-// export const contractAddress = "0xAB1A8eFC31758611a98c8433512A549C67587994"
-export const contractAddress = "0xaC154D35699A141640d49bcBdbbEa1b093b7b7Ee"
+export const contractAddress = "0x0f88056bFc24fB6807284c41f169F87Ccd3DFA9f"
 
 export const stakingUrl = "https://stake.kammorostoken.com"
