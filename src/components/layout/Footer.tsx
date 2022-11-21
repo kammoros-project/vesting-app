8ee4ab04e8d5b92c3832e3037b2cb9f5bb8f454c
Bob <heycryptobob@gmail.com>
2022-11-21 17:07:16 +0800

    seo

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,7 +2,7 @@ import Container from "./Container";
 
 function Footer() {
     return (
-        <div className="bg-slate-900 text-white py-4 mt-32">
+        <div className="bg-white text-slate-800 py-4 mt-32">
             <Container>
                 <div>Built by <a href="https://t.me/B10KDeveloper" target={"_blank"} rel="noreferrer" className="hover:underline">[B10K.IO]</a></div>
             </Container>
